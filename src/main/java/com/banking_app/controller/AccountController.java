6b1cd6bf6ea0fbf3b0bdae01a9941fcd91bbdc24
anoopreddy-padi <anoopreddy.padi@gmail.com>
2025-12-30 11:29:26 +0530

    imports management

--- src/main/java/com/banking_app/controller/AccountController.java
+++ src/main/java/com/banking_app/controller/AccountController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/banking_app/service/impl/AccountServiceImpl.java
+++ src/main/java/com/banking_app/service/impl/AccountServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import com.banking_app.dto.AccountDto;
